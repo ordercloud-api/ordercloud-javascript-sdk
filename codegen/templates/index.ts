e3177edd8c16faa0117df26f4453386082c5a542
Crhistian Ramirez <cramirez@four51.com>
2021-02-27 10:45:06 -0600

    :bug: OrderCloudError not exported from public API

--- codegen/templates/index.ts
+++ codegen/templates/index.ts
@@ -1,5 +1,6 @@
 import Configuration from './Configuration'
 export { Configuration }
+import OrderCloudError from './utils/OrderCloudError'
+export { OrderCloudError }
 export * from './api/index'
 export * from './models/index'
-export * from './utils/OrderCloudError'
