1093db0d258a8b266e2111d53df2c3f23de35336
Erin <erincdustin@gmail.com>
2025-06-12 13:06:41 -0700

    ⏩ regen to API v1.0.402

--- src/models/RemovedPromo.ts
+++ src/models/RemovedPromo.ts
@@ -1,7 +1,31 @@
 
-export interface RemovedPromo {
+export interface RemovedPromo<TRemovedPromoXp = any> {
     ErrorCode?: string
-    ID?: string
-    LineItemID?: string
     Reason?: string
+    LineItemID?: string
+    Amount?: number
+    ID?: string
+    LineItemLevel?: boolean
+    Code?: string
+    Name?: string
+    RedemptionLimit?: number
+    RedemptionLimitPerUser?: number
+    readonly RedemptionCount?: number
+    QuantityLimitPerOrder?: number
+    ItemLimitPerOrder?: number
+    ItemSortBy?: string
+    Description?: string
+    FinePrint?: string
+    StartDate?: string
+    ExpirationDate?: string
+    EligibleExpression?: string
+    ValueExpression?: string
+    CanCombine?: boolean
+    AllowAllBuyers?: boolean
+    OwnerID?: string
+    AutoApply?: boolean
+    Active?: boolean
+    UseIntegration?: boolean
+    Priority?: number
+    xp?: TRemovedPromoXp
 }
